53603495ae9ce983c450ecab272261b9f1b3ac72
Solomka-0 <krasjuk2@gmail.com>
2025-07-23 22:08:52 +0300

    feature: добавил tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
 })
